5e5b799754a3f3eb911441baf4234b020ebb41e2
Koundinya21 <koundinyamiddela21@gmail.com>
2025-04-26 21:54:43 +0530

    commit message

---
+++ frontend/notes-app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Add jsx, ts, tsx support
+  theme: {
+    extend: {
+      //colors used in project
+      colors:{
+        primary:"#2B85FF",
+        secondary:"Ef863E"
+      }
+    },
+  },
+  plugins: [],
+};
